ff0d33291a3001723146946b2bbd1f95b9a91806
island255 <jiaang@stu.xjtu.edu.cn>
2020-09-26 17:27:35 +0800

    update Calculate_similarities.py

--- backup/Calculate_similarities.py
+++ backup/Calculate_similarities.py
@@ -38,7 +38,8 @@ def get_pickle_content(paths):
         pickle_file = open(path, "r")
         feature_vector = pickle.load(pickle_file)
         pickle_file.close()
-        feature_vectors = sparse.vstack((feature_vectors, feature_vector))
+        # print(type(pickle_file))
+        feature_vectors = sparse.vstack((feature_vectors, feature_vector), format="csr")
     return feature_vectors
 
 
@@ -59,8 +60,90 @@ def read_graph_names(file_name):
     graph_names = []
     with open(file_name, "r") as f:
         for line in f.readlines():
-            graph_names.append(line.strip("/").split("/"))
+            graph_names.append(line.split(",")[-1].strip("/").strip("\n").split("/"))
     return graph_names
+
+
+def separate_vectors_by_names(graph_names, graph_vectors):
+    """
+    separate graph names and vectors according their projects
+    :param graph_names:
+    :param graph_vectors:
+    :return:
+    """
+
+    print("start separating ...")
+    graph_names_separate = []
+    graph_vectors_separate = []
+
+    print(len(graph_names), graph_vectors._shape)
+    for i in range(len(graph_names)):
+        start_index = 0
+        end_index = 0
+        if i != 0 and (graph_names[i][0] != graph_names[i - 1][0] or graph_names[i][1] != graph_names[i - 1][1]):
+            print("separating num: " + str(i))
+            print ("separating name: " + graph_names[i][0] + "-" + graph_names[i][1])
+            end_index = i
+            graph_names_separate.append(graph_names[start_index: end_index])
+            graph_vectors_separate.append(graph_vectors[start_index: end_index])
+            start_index = i
+
+        if i == len(graph_names) - 1:
+            end_index = len(graph_names)
+            graph_names_separate.append(graph_names[start_index: end_index])
+            graph_vectors_separate.append(graph_vectors[start_index: end_index])
+
+        if i % 1000 == 0:
+            print(i)
+
+    return graph_names_separate, graph_vectors_separate
+
+
+def write_similarity_results(gram_matrix, graph_name_pair):
+    """
+    writing results of similarities between groups
+    :param gram_matrix:
+    :param graph_name_pair:
+    :return:
+    """
+    filename = graph_name_pair[0][0][0] + "_" + graph_name_pair[0][0][1] + "-" + graph_name_pair[1][0][0] + "_" + \
+               graph_name_pair[1][0][1]
+    dir_name = "similarities"
+    if os.path.exists(dir_name) is False:
+        os.mkdir(dir_name)
+    with open(dir_name + "/" + filename, "w") as f:
+        graph_name_pair_0 = ""
+        for i in range(len(graph_name_pair[0])):
+            graph_name_pair_0 = graph_name_pair_0 + "_".join(graph_name_pair[0][i]) + ","
+        graph_name_pair_0.strip(",")
+        graph_name_pair_1 = ""
+        for i in range(len(graph_name_pair[1])):
+            graph_name_pair_1 = graph_name_pair_1 + "_".join(graph_name_pair[1][i]) + ","
+        graph_name_pair_1.strip(",")
+
+        f.write(graph_name_pair_0 + "\n")
+        f.write(graph_name_pair_1 + "\n")
+        for line in gram_matrix:
+            f.write(",".join(line) + "\n")
+
+
+def calculate_similarities(graph_names_separate, graph_vectors_separate):
+    """
+    calculate similarities between groups
+    :param graph_names_separate:
+    :param graph_vectors_separate:
+    :return:
+    """
+    for i in range(len(graph_names_separate)):
+        for j in range(len(graph_names_separate)):
+            graph_name_pair = (graph_names_separate[i], graph_names_separate[j])
+            print ("process groups of " + graph_name_pair[0][0][0] + "_" + graph_name_pair[0][0][1] + "-" +
+                   graph_name_pair[1][0][0] + "_" + graph_name_pair[1][0][1])
+            gram_matrix = graph_vectors_separate[i].dot(graph_vectors_separate[j].T)
+            gram_matrix = gram_matrix.toarray()
+            gram_matrix = aux.normalize_gram_matrix(gram_matrix)
+
+            write_similarity_results(gram_matrix, graph_name_pair)
 
 
 def main():
@@ -72,20 +155,23 @@ def main():
         print("read graph vectors and aggregate to one pickle")
         feature_vector_paths = get_graph_file_path("feature_vectors")
         graph_vectors = get_pickle_content(feature_vector_paths)
-        print(type(graph_vectors))
+        iterations = 20
+        graph_vectors = m.sqrt(1.0 / iterations) * graph_vectors
+        # print("convert coo_matrix to csr_matrix")
+        # graph_vectors = graph_vectors.tocsr()
         write_pickle(graph_vectors, "graph_vectors")
 
-    print("calculate gram matrix for graph vectors")
-    iterations = 20
-    feature_vectors = m.sqrt(1.0 / iterations) * graph_vectors
-    # gram_matrix = feature_vectors.dot(feature_vectors.T)
-    # gram_matrix = gram_matrix.toarray()
-    # normalize_gram_matrix = True
-    # if normalize_gram_matrix:
-    #     gram_matrix = aux.normalize_gram_matrix(gram_matrix)
+    print(type(graph_vectors))
+    print("calculate similarity matrix for graph vectors")
 
-    print("save gram matrix as pickle")
-    # write_pickle(gram_matrix, "gram_matrix")
+    graph_names = read_graph_names("LLVM_IR_to_Graph.txt")
+    print(graph_names[0])
+
+    print("separate vectors according their groups")
+    graph_names_separate, graph_vectors_separate = separate_vectors_by_names(graph_names, graph_vectors)
+
+    print("calculate similarities between groups")
+    calculate_similarities(graph_names_separate, graph_vectors_separate)
 
 
 if __name__ == '__main__':
